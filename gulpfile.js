b497be808968fde4ca4aa448e9a16780fd91b066
Michael Hopfner <mhopfner@gmx.at>
2016-09-20 15:46:51 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+"use strict"
+
+var gulp = require('gulp');
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var buffer = require("vinyl-buffer")
+var glob = require("glob")
+
+
+gulp.task("default", function() {
+	gulp.watch("src/**/*.js", ["browserify"])
+	gulp.start("browserify")
+	return copyCss()
+})
+
+function copyCss() {
+	return gulp.src("node_modules/live-remote-params/target/css/params.css")
+	.pipe(gulp.dest("target"))
+}
+
+gulp.task("browserify", function() {
+	return browserify()
+	.add(glob.sync("./js/**/*.js"))
+	.bundle()
+	.on("error", function(e) {
+		console.log("Error in browserify: " + e.message)
+	})
+	.pipe(source("live-remote-js-example.js"))
+	.pipe(gulp.dest("target"))
+})
---
+++ js/live-remote-js-example.js
@@ -0,0 +1,36 @@
+"use strict"
+
+var RemoteApi = require("live-remote-api").RemoteApi  
+var params = require("live-remote-params").params  
+var $ = require("jquery")  
+
+RemoteApi.onOpen(function() {  
+	RemoteApi.create("live_set tracks 3 mixer_device panning", function(err, api) {  
+		var knob = new params.PanKnob()  
+		knob.api(api)  
+		$(document.body).append(knob.div())  
+	})
+
+	RemoteApi.create("live_set master_track mixer_device cue_volume", function(err, api) {  
+		var knob = new params.Knob()  
+		knob.api(api)  
+		$(document.body).append(knob.div())  
+	})  
+
+	RemoteApi.create("live_set master_track mixer_device crossfader", function(err, api) {  
+		var knob = new params.HSlider()  
+		knob.api(api)  
+		$(document.body).append(knob.div())  
+	})  
+
+	RemoteApi.create("live_set master_track mixer_device volume", function(err, api) {  
+		var slider = new params.Slider()  
+		slider.api(api)  
+		$(document.body).append(slider.div())  
+	})  
+	RemoteApi.create("live_set tracks 3 mixer_device track_activator", function(err, api) {  
+		var t = new params.Toggle()  
+		t.api(api)  
+		$(document.body).append(t.div())  
+	})  
+})
